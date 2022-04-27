61bd157acd31ad4693cc32c2216748b63f63a8ee
Julius Heinzinger <julius.heinzinger@gmail.com>
2022-04-27 12:10:13 +0200

    Parse args immediately and return the parsed args

--- src/cli/cli.py
+++ src/cli/cli.py
@@ -1,11 +1,15 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 
 
-def get_parser() -> ArgumentParser:
+def parse_args() -> Namespace:
     parser = ArgumentParser("pdf2gtfs")
     _add_required_arguments(parser)
     _add_optional_arguments(parser)
-    return parser
+    return parser.parse_args()
+
+
+def update_config(parser: ArgumentParser):
+    ...
 
 
 def _add_required_arguments(parser: ArgumentParser):
@@ -17,7 +21,7 @@ def _add_required_arguments(parser: ArgumentParser):
 def _add_optional_arguments(parser: ArgumentParser):
     parser.add_argument("--time_format", type=str,
                         help="The format of the timestrings of the pdf table")
-    parser.add_argument("--header_identifier", type=str,
-                        help="Which identifier to use for the headers")
-    parser.add_argument("--repeat_identifier", type=str,
+    parser.add_argument("--header_identifier", type=list,
+                        help="Which identifiers are used for the headers")
+    parser.add_argument("--repeat_identifier", type=list,
                         help="How repeating times are identified")
