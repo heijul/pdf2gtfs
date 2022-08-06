7657c5965b8de7626d2b7b18dfbb8166f4301da2
Julius Heinzinger <julius.heinzinger@gmail.com>
2022-08-06 13:18:31 +0200

    Add calendar tests

--- src/test/test_gtfs_output/test_calendar.py
+++ src/test/test_gtfs_output/test_calendar.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from datastructures.gtfs_output.calendar import (
-    CalendarEntry, WEEKDAY_NAMES, DayIsActive)
+    CalendarEntry, WEEKDAY_NAMES, DayIsActive, Calendar)
 from test_gtfs_output import GTFSOutputBaseClass
 
 
@@ -54,3 +54,42 @@ class TestCalendar(GTFSOutputBaseClass):
     @classmethod
     def setUpClass(cls, name="calendar.txt") -> None:
         super().setUpClass(name)
+
+    def test_try_add(self):
+        days = ["0", "1", "2"]
+        annots = set()
+        c = Calendar()
+        self.assertEqual(0, len(c.entries))
+        c.try_add(days, annots)
+        self.assertEqual(1, len(c.entries))
+        c.try_add(days, annots)
+        self.assertEqual(1, len(c.entries))
+        c.try_add(days, {"test_annotation"})
+        self.assertEqual(2, len(c.entries))
+        c.try_add(days + ["h"], {"test_annotation"})
+        self.assertEqual(3, len(c.entries))
+
+    def test_get(self):
+        c = Calendar()
+        days = ["0", "5", "3"]
+        e = CalendarEntry(days, None)
+        c.try_add(days, set())
+        self.assertEqual(e, c.get(e))
+        self.assertEqual(e, c.entries[0])
+        self.assertNotEqual(id(e), id(c.get(e)))
+
+    def test_group_by_holiday(self):
+        c = Calendar()
+        for i in range(7):
+            if i % 2:
+                c.try_add(["h"], {"holiday", str(i)})
+            else:
+                c.try_add(["1", "2"], {str(i)})
+        on_holidays, no_holidays = c.group_by_holiday()
+        self.assertEqual(len(c.entries), len(on_holidays) + len(no_holidays))
+        self.assertEqual(3, len(on_holidays))
+        self.assertEqual(4, len(no_holidays))
+        for entry in on_holidays:
+            self.assertTrue(entry.on_holidays)
+        for entry in no_holidays:
+            self.assertFalse(entry.on_holidays)
