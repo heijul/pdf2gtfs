a27f08c3c6cf00dc76985f1b8da7044c476f63ea
Julius Heinzinger <julius.heinzinger@gmail.com>
2022-09-05 12:14:05 +0200

    Remove todo

--- src/test/test_timetable/__init__.py
+++ src/test/test_timetable/__init__.py
@@ -2,7 +2,6 @@ from datastructures.timetable.stops import Stop
 
 
 def create_stops(count: int = 3):
-    # TODO: Move to test_timetable/stops
     stops = []
     for i in range(count):
         stops.append(Stop(f"stop{i}", i))
