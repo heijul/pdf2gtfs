e1f774827e5ba17851485ccb1eedd57e35f0139c
Julius Heinzinger <julius.heinzinger@gmail.com>
2022-09-05 07:01:06 +0200

    Cleanup deltas Property

--- src/datastructures/timetable/entries.py
+++ src/datastructures/timetable/entries.py
@@ -45,30 +45,47 @@ class TimeTableRepeatEntry(TimeTableEntry):
 
     @property
     def deltas(self) -> list[int]:
-        def to_int_list(_value_str: str) -> list[int]:
-            try:
-                _values = list(map(int, _value_str.split("-")))
-            except ValueError:
-                return []
-            if len(_values) == 2:
-                # TODO: Does range make sense here?
-                #  Should probably be done in gtfs_output
-                return list(range(_values[0], _values[1] + 1))
-
-            return [_values[0]] if len(_values) else []
-
         start = False
-        for value_str in self.values.values():
-            value_str = value_str.lower().strip()
-            if value_str in Config.repeat_identifier:
+        for value in self.values.values():
+            value = value.lower().strip()
+            if value in Config.repeat_identifier:
                 start = True
                 continue
-            value_str = value_str.replace(" ", "")
-            if not start or not value_str:
+            value = value.replace(" ", "")
+            if not start or not value:
                 continue
 
-            value = to_int_list(value_str)
-            if not value:
+            int_list = self._to_int_list(value)
+            if not int_list:
                 continue
-            return value
+            return int_list
+        return []
+
+    @staticmethod
+    def _to_int_list(value_str: str) -> list[int]:
+        """ Turn the value_str to a list of ints, depending its format.
+        If it is of the form:
+            - "x,y,..." it returns [x, y, ...]
+            - "x-y" it returns [x, x1, ..., xn, y] where x1 = x + 1 if
+              x + 1 < y. E.g. "7-9" returns [7, 8, 9]
+            - Otherwise it returns a single element list of int(value_str)
+        """
+        values: dict[str: list[int]] = {}
+        split_chars = [",", "-"]
+        try:
+            for char in split_chars:
+                values[char] = []
+                values[char] = list(map(int, value_str.split(char)))
+        except ValueError:
+            pass
+        if values[","]:
+            return values[","]
+        if values["-"]:
+            if len(values["-"]) == 2:
+                return list(range(values["-"][0], values["-"][1] + 1))
+            return values["-"]
+        try:
+            return [int(value_str)]
+        except ValueError:
+            pass
         return []
