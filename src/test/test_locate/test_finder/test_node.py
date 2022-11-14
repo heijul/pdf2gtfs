65a3808184ee25a3dd3afd37886b95a376ec0a7d
Julius Heinzinger <julius.heinzinger@gmail.com>
2022-11-14 15:01:02 +0100

    Create empty test files of missing tests

---
+++ src/test/test_locate/test_finder/test_node.py
@@ -0,0 +1,36 @@
+from unittest import TestCase
+
+from config import Config
+from locate.finder import Nodes
+from test_locate.test_finder import (
+    get_stops_and_dummy_df, get_stops_from_stops_list)
+
+
+class TestNode(TestCase):
+    def setUp(self) -> None:
+        Config.average_speed = 10
+        stops_list, self.df = get_stops_and_dummy_df()
+        self.stops = get_stops_from_stops_list(stops_list)
+        self.nodes = Nodes(self.df, self.stops)
+
+    def test_get_neighbors(self) -> None:
+        node_heap = self.nodes._node_heap
+
+        i = 0
+        while node_heap.first:
+            with self.subTest(i=i):
+                node = node_heap.pop()
+                if node.stop == self.stops.last:
+                    continue
+                neighbors = node.get_close_neighbors()
+                # Check that the neighbors are close
+                for neigbor in neighbors:
+                    self.assertTrue(node.close_nodes(neigbor))
+                # Check that neighbors all have the correct stop.
+                for neighbor in neighbors:
+                    self.assertEqual(node.stop.next, neighbor.stop)
+                i += 1
+
+    def test_update_parent_if_lower_cost(self) -> None:
+        # TODO:
+        pass
---
+++ src/test/test_locate/test_finder/test_stop.py
@@ -0,0 +1,59 @@
+from unittest import mock, TestCase
+
+from config import Config
+from datastructures.gtfs_output.stop_times import Time
+from test_locate.test_finder import get_stops_from_stops_list
+
+
+class TestStop(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        Config.load_default_config()
+
+    def setUp(self) -> None:
+        stop_list = [("0", "stop_0"), ("1", "stop_1")]
+        self.stops = get_stops_from_stops_list(stop_list)
+        Config.average_speed = 15
+
+    def test_exists(self) -> None:
+        ...
+
+    @mock.patch("locate.finder.stops.Stops.get_avg_time_between")
+    def test_set_distance_bounds(self, get_avg_time_between) -> None:
+        Config.average_speed = 15
+        min_dist = Config.min_travel_distance
+        Config.average_travel_distance_offset = 2
+        get_avg_time_between.side_effect = [
+            Time(minutes=minutes) for minutes in [1, 2, 12, 0]]
+        expected_lower = [min_dist, min_dist, 2500, min_dist]
+        expected_mid = [250, 500, 3000, 0]
+        expected_upper = [750, 1000, 3500, 0]
+        stop_1 = self.stops.first
+        for i in range(3):
+            with self.subTest(i=i):
+                lower, mid, upper = stop_1._get_distance_bounds()
+                self.assertEqual(expected_lower[i], lower)
+                self.assertEqual(expected_mid[i], mid)
+                self.assertEqual(expected_upper[i], upper)
+
+    def test_before(self) -> None:
+        stop_1 = self.stops.first
+        stop_2 = stop_1.next
+        stop_3 = stop_2.next
+        self.assertTrue(stop_1.before(stop_2))
+        self.assertFalse(stop_2.before(stop_1))
+        self.assertTrue(stop_1.before(stop_3))
+        self.assertFalse(stop_2.before(stop_2))
+
+    def test_after(self) -> None:
+        stop_1 = self.stops.first
+        stop_2 = stop_1.next
+        stop_3 = stop_2.next
+        self.assertFalse(stop_1.after(stop_2))
+        self.assertTrue(stop_2.after(stop_1))
+        self.assertTrue(stop_3.after(stop_1))
+        self.assertFalse(stop_2.after(stop_2))
+
+    def test_avg_time_to_next(self) -> None:
+        # TODO: Ignore for now cause we'd need a working handler + stop_times
+        pass
