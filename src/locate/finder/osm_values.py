360bf970cde8ddf9882568404989e017775e865a
Julius Heinzinger <julius.heinzinger@gmail.com>
2022-11-16 12:30:47 +0100

    Add tests for osm_values; Refactor osm_values

--- src/locate/finder/osm_values.py
+++ src/locate/finder/osm_values.py
@@ -14,9 +14,12 @@ GoodValues: TypeAlias = dict[OSMKey: dict[str: int]]
 BadValues: TypeAlias = dict[OSMKey: list[str]]
 
 
+# TODO: This works but is done waaay too complicated.
+
+
 def get_all_cat_scores() -> tuple[GoodValues, BadValues]:
     """ Return all values, which make a node a good or bad match. """
-    osm_value = get_osm_value()
+    osm_value = get_osm_values()[Config.gtfs_routetype]()
     return osm_value.good_values, osm_value.bad_values
 
 
@@ -38,39 +41,27 @@ def get_osm_values() -> dict[str: OSMValue]:
             "Monorail": Monorail}
 
 
-def get_osm_value() -> OSMValue:
-    """ Get the values for the current routetype. """
-    return get_osm_values()[Config.gtfs_routetype]()
-
-
 class OSMValue:
     """ Base class to enable querying for the values of a specific key. """
     bad_values: BadValues
 
     def __init__(self) -> None:
-        self.good_values = self._get_good_values()
-        self.bad_values = self._get_bad_values()
-
-    def _get_good_values(self) -> GoodValues:
-        return {}
-
-    def _get_bad_values(self) -> BadValues:
-        return {}
+        self.good_values = {}
+        self.bad_values = {}
 
 
 class Tram(OSMValue):
     """ Trams are rail-based, so rail-specific keys are considered as well. """
 
-    def _get_good_values(self) -> GoodValues:
-        return {"tram": {"yes": 0},
-                "light_rail": {"yes": 1},
-                "station": {"light_rail": 1},
-                "railway": {"tram_stop": 0, "halt": 2,
-                            "station": 2, "platform": 2},
-                "train": {"yes": 2}}
-
-    def _get_bad_values(self) -> BadValues:
-        return {"tram": ["no"]}
+    def __init__(self) -> None:
+        super().__init__()
+        self.good_values = {"tram": {"yes": 0},
+                            "light_rail": {"yes": 1},
+                            "station": {"light_rail": 1},
+                            "railway": {"tram_stop": 0, "halt": 2,
+                                        "station": 2, "platform": 2},
+                            "train": {"yes": 2}}
+        self.bad_values = {"tram": ["no"]}
 
 
 class StreetCar(Tram):
@@ -83,30 +74,28 @@ class LightRail(OSMValue):
     """ Light rail vehicles are rail based like trams,
     but have their own specific values. """
 
-    def _get_good_values(self) -> GoodValues:
-        return {
-            "light_rail": {"yes": 0},
-            "station": {"light_rail": 0},
-            "tram": {"yes": 0},
-            "railway": {"tram_stop": 0, "halt": 1,
-                        "station": 1, "platform": 1},
-            "train": {"yes": 1}}
-
-    def _get_bad_values(self) -> BadValues:
-        return {"light_rail": ["no"]}
+    def __init__(self) -> None:
+        super().__init__()
+        self.good_values = {"light_rail": {"yes": 0},
+                            "station": {"light_rail": 0},
+                            "tram": {"yes": 0},
+                            "railway": {"tram_stop": 0, "halt": 1,
+                                        "station": 1, "platform": 1},
+                            "train": {"yes": 1}}
+        self.bad_values = {"light_rail": ["no"]}
 
 
 class Subway(OSMValue):
     """ Subways have their own set of keys, but are rail based as well. """
 
-    def _get_good_values(self) -> GoodValues:
-        return {"subway": {"yes": 0},
-                "train": {"yes": 1},
-                "station": {"subway": 0, "train": 1},
-                "railway": {"halt": 0, "station": 1, "platform": 1}}
-
-    def _get_bad_values(self) -> BadValues:
-        return {"subway": ["no"]}
+    def __init__(self) -> None:
+        super().__init__()
+        self.good_values = {"subway": {"yes": 0},
+                            "train": {"yes": 1},
+                            "station": {"subway": 0, "train": 1},
+                            "railway": {"halt": 0, "station": 1,
+                                        "platform": 1}}
+        self.bad_values = {"subway": ["no"]}
 
 
 class Metro(Subway):
@@ -117,49 +106,48 @@ class Metro(Subway):
 class Rail(OSMValue):
     """ Any rail based keys/values. """
 
-    def _get_good_values(self) -> GoodValues:
-        return {"train": {"yes": 0},
-                "station": {"train": 0},
-                "railway": {"halt": 0, "station": 1, "platform": 1}}
-
-    def _get_bad_values(self) -> BadValues:
-        return {"train": ["no"]}
+    def __init__(self) -> None:
+        super().__init__()
+        self.good_values = {"train": {"yes": 0},
+                            "station": {"train": 0},
+                            "railway": {"halt": 0, "station": 1,
+                                        "platform": 1}}
+        self.bad_values = {"train": ["no"]}
 
 
 class Bus(OSMValue):
     """ Busses are street-based, but may use trolleybus stations as well. """
 
-    def _get_good_values(self) -> GoodValues:
-        return {"bus": {"yes": 0},
-                "amenity": {"bus_station": 0},
-                "highway": {"bus_stop": 0, "platform": 1},
-                "trolleybus": {"yes": 2}}
-
-    def _get_bad_values(self) -> BadValues:
-        return {"bus": ["no"]}
+    def __init__(self) -> None:
+        super().__init__()
+        self.good_values = {"bus": {"yes": 0},
+                            "amenity": {"bus_station": 0},
+                            "highway": {"bus_stop": 0, "platform": 1},
+                            "trolleybus": {"yes": 2}}
+        self.bad_values = {"bus": ["no"]}
 
 
 class Ferry(OSMValue):
     """ Ferry is the only water-based transport vehicle. """
 
-    def _get_good_values(self) -> GoodValues:
-        return {"ferry": {"yes": 0},
-                "amenity": {"ferry_terminal": 0}}
-
-    def _get_bad_values(self) -> BadValues:
-        return {"ferry": ["no"]}
+    def __init__(self) -> None:
+        super().__init__()
+        self.good_values = {"ferry": {"yes": 0},
+                            "amenity": {"ferry_terminal": 0}}
+        self.bad_values = {"ferry": ["no"]}
 
 
 class CableTram(OSMValue):
     """ Cabletram is rail-based. """
 
-    def _get_good_values(self) -> GoodValues:
-        return {"tram": {"yes": 1},
-                "light_rail": {"yes": 1},
-                "train": {"yes": 2},
-                "railway": {"halt": 2, "tram_stop": 1,
-                            "station": 2, "platform": 2},
-                "station": {"light_rail": 3}}
+    def __init__(self) -> None:
+        super().__init__()
+        self.good_values = {"tram": {"yes": 1},
+                            "light_rail": {"yes": 1},
+                            "train": {"yes": 2},
+                            "railway": {"halt": 2, "tram_stop": 1,
+                                        "station": 2, "platform": 2},
+                            "station": {"light_rail": 3}}
 
 
 class AerialLift(OSMValue):
@@ -178,33 +166,33 @@ class SuspendedCableCar(AerialLift):
 class Funicular(OSMValue):
     """ Funicular is rail-based. """
 
-    def _get_good_values(self) -> GoodValues:
-        return {"railway": {"funicular": 0, "light_rail": 1},
-                "station": {"funicular": 0},
-                "light_rail": {"yes": 2}}
+    def __init__(self) -> None:
+        super().__init__()
+        self.good_values = {"railway": {"funicular": 0, "light_rail": 1},
+                            "station": {"funicular": 0},
+                            "light_rail": {"yes": 2}}
 
 
 class Trolleybus(OSMValue):
     """ Trolleybus is both highway-based and has its own keys/values. """
 
-    def _get_good_values(self) -> GoodValues:
-        return {"trolleybus": {"yes", 0},
-                "bus": {"yes": 1},
-                "amenity": {"bus_station": 1},
-                "highway": {"bus_stop": 1, "platform": 1}}
-
-    def _get_bad_values(self) -> BadValues:
-        return {"trolleybus": ["no"]}
+    def __init__(self) -> None:
+        super().__init__()
+        self.good_values = {"trolleybus": {"yes", 0},
+                            "bus": {"yes": 1},
+                            "amenity": {"bus_station": 1},
+                            "highway": {"bus_stop": 1, "platform": 1}}
+        self.bad_values = {"trolleybus": ["no"]}
 
 
 class Monorail(OSMValue):
     """ Monorail is both rail-based and has its own keys/values. """
 
-    def _get_good_values(self) -> GoodValues:
-        return {"monorail": {"yes": 0},
-                "station": {"monorail": 1},
-                "railway": {"halt": 1, "platform": 1, "station": 2},
-                "light_rail": {"yes": 2}}
-
-    def _get_bad_values(self) -> BadValues:
-        return {"monorail": ["no"]}
+    def __init__(self) -> None:
+        super().__init__()
+        self.good_values = {"monorail": {"yes": 0},
+                            "station": {"monorail": 1},
+                            "railway": {"halt": 1, "platform": 1,
+                                        "station": 2},
+                            "light_rail": {"yes": 2}}
+        self.bad_values = {"monorail": ["no"]}
--- src/test/test_locate/test_finder/test_osm_values.py
+++ src/test/test_locate/test_finder/test_osm_values.py
@@ -1,5 +1,26 @@
+from config import Config
+from datastructures.gtfs_output.routes import RouteType
 from test import P2GTestCase
+import locate.finder.osm_values as osmv
 
 
 class TestOSMValues(P2GTestCase):
-    ...
+    def test_get_all_cat_scores(self) -> None:
+        Config.gtfs_routetype = "Tram"
+        cat_scores1 = osmv.get_all_cat_scores()
+        Config.gtfs_routetype = "Bus"
+        cat_scores2 = osmv.get_all_cat_scores()
+        self.assertNotEqual(cat_scores1, cat_scores2)
+        self.assertEqual(cat_scores1[0], osmv.Tram().good_values)
+        self.assertEqual(cat_scores1[1], osmv.Tram().bad_values)
+        self.assertEqual(cat_scores2[0], osmv.Bus().good_values)
+        self.assertEqual(cat_scores2[1], osmv.Bus().bad_values)
+
+    def test_get_osm_values(self) -> None:
+        values = osmv.get_osm_values()
+        for i, name in enumerate(values):
+            with self.subTest(i=i):
+                try:
+                    RouteType[name]
+                except KeyError:
+                    self.fail(f"KeyError raised for {name}")
