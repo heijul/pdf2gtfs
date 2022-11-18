383baa81025dd53b6fb97a34a98cf0088c70cf10
Julius Heinzinger <julius.heinzinger@gmail.com>
2022-11-18 20:54:12 +0100

    Remove test skeletons

--- src/test/test_datastructures/test_timetable/test_table.py
+++ src/test/test_datastructures/test_timetable/test_table.py
@@ -1,34 +1,11 @@
 from test import P2GTestCase
 
 
-class TestStopList(P2GTestCase):
-    def test_all_stops(self) -> None:
-        ...
-
-    def test_stops(self) -> None:
-        ...
-
-    def test_add_stop(self) -> None:
-        ...
-
-    def test_get_from_id(self) -> None:
-        ...
-
-    def test_add_annotation(self) -> None:
-        ...
-
-    def test_clean(self) -> None:
-        ...
-
-
 class TestTimeTable(P2GTestCase):
     def test_detect_connection(self) -> None:
         ...
 
     def test_from_pdf_table(self) -> None:
-        ...
-
-    def test_print(self) -> None:
         ...
 
     def test_clean_values(self) -> None:
