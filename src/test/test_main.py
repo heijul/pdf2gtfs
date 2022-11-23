eaea5b45852ce4e83f39da3b8e8340dfac0e46f8
Julius Heinzinger <julius.heinzinger@gmail.com>
2022-11-23 23:52:38 +0100

    Rename; Add missing test skeletons

--- src/test/test_main.py
+++ src/test/test_main.py
@@ -1,5 +1,15 @@
-from test import P2GTestCase
+from unittest import TestCase
 
 
-class TestMain(P2GTestCase):
-    ...
+class Test(TestCase):
+    def test_get_timetables(self) -> None:
+        ...
+
+    def test_generate_gtfs(self) -> None:
+        ...
+
+    def test_detect_locations(self) -> None:
+        ...
+
+    def test_main(self) -> None:
+        ...
