82e33709d2c564724340c55581d0e5b2adfe6623
Julius Heinzinger <julius.heinzinger@gmail.com>
2023-04-12 23:37:49 +0200

    Fix EmptyField insertion

--- src/pdf2gtfs/datastructures/table/table.py
+++ src/pdf2gtfs/datastructures/table/table.py
@@ -2,9 +2,9 @@ from __future__ import annotations
 
 from itertools import pairwise
 from operator import attrgetter
-from typing import Callable, TypeVar
+from typing import Callable, Iterable, TypeVar
 
-from more_itertools import peekable, split_when, spy
+from more_itertools import always_iterable, peekable, split_when, spy
 
 from pdf2gtfs.datastructures.pdftable.bbox import BBox
 from pdf2gtfs.datastructures.table.bounds import select_adjacent_fields
@@ -23,6 +23,17 @@ Col = TypeVar("Col")
 Row = TypeVar("Row")
 Cols = TypeVar("Cols")
 Rows = TypeVar("Rows")
+
+
+def merge_overlapping_fields(o: Orientation, fields: Fs) -> None:
+    if o == V:
+        groups = fields_to_rows(fields, link_rows=False)
+    else:
+        groups = fields_to_cols(fields, link_cols=False)
+    for group in groups:
+        field = group[0]
+        for field2 in group[1:]:
+            field.merge(field2)
 
 
 def merge_small_fields(o: Orientation, ref_fields: Fs, fields: Fs
@@ -58,6 +69,51 @@ def merge_small_fields(o: Orientation, ref_fields: Fs, fields: Fs
         f2 = f1.get_neighbor(n.upper)
 
 
+def insert_empty_fields_from_map(
+        o: Orientation, ref_fields: Fs, fields: Fs) -> F:
+    # Add fields at the start and between other fields.
+    i = 0
+    field_count = 0
+    for ref_field in ref_fields:
+        if i >= len(fields):
+            break
+        field_count += 1
+        field = fields[i]
+        if ref_field.is_overlap(o, field, 0.8):
+            i += 1
+            continue
+        e = EmptyField()
+        if o == H:
+            e.bbox = BBox(ref_field.bbox.x0, field.bbox.y0,
+                          ref_field.bbox.x1, field.bbox.y1)
+        else:
+            e.bbox = BBox(field.bbox.x0, ref_field.bbox.y0,
+                          field.bbox.x1, ref_field.bbox.y1)
+
+        field.set_neighbor(o.lower, e)
+
+    # Add empty nodes at the end.
+    field = fields[-1]
+    while field_count < len(ref_fields):
+        e = EmptyField()
+        ref_field = ref_fields[field_count]
+        if o == H:
+            e.bbox = BBox(ref_field.bbox.x0, field.bbox.y0,
+                          ref_field.bbox.x1, field.bbox.y1)
+        else:
+            e.bbox = BBox(field.bbox.x0, ref_field.bbox.y0,
+                          field.bbox.x1, ref_field.bbox.y1)
+        field.set_neighbor(o.upper, e)
+        field_count += 1
+        field = e
+
+    # Return head
+    field = fields[0]
+    while field.has_neighbors(d=o.lower):
+        field = field.get_neighbor(o.lower)
+    return field
+
+
 class Table(QuadLinkedList[F, OF]):
     def __init__(self, first_node: F, last_node: F):
         super().__init__(first_node, last_node)
@@ -69,9 +125,9 @@ class Table(QuadLinkedList[F, OF]):
         :param fields:
         :return:
         """
-        cols = datafields_to_cols(fields)
-        rows = datafields_to_rows(fields)
-        insert_missing_empty_fields(rows, cols)
+        cols = fields_to_cols(fields)
+        rows = fields_to_rows(fields)
+        link_rows_and_cols(rows, cols)
         t = Table(cols[0][0], rows[-1][-1])
         return t
 
@@ -79,33 +135,6 @@ class Table(QuadLinkedList[F, OF]):
         row_bbox = self.get_bbox_of(self.row(node))
         col_bbox = self.get_bbox_of(self.col(node))
         return BBox(col_bbox.x0, row_bbox.y0, col_bbox.x1, row_bbox.y1)
-
-    def insert_empty_fields_from_map(
-            self, o: Orientation, ref_fields: Fs, fields: Fs) -> F:
-        i = 0
-        field_count = 0
-        for ref_field in ref_fields:
-            if i >= len(fields):
-                break
-            field_count += 1
-            field = fields[i]
-            if ref_field.is_overlap(o, field, 0.8):
-                i += 1
-                continue
-            e = EmptyField()
-            field.set_neighbor(o.lower, e)
-
-        # Add empty nodes at the end.
-        while field_count < len(ref_fields):
-            e = EmptyField()
-            fields[-1].set_neighbor(o.upper, e)
-            field_count += 1
-
-        field = fields[0]
-        while True:
-            if not field.get_neighbor(o.lower):
-                return field
-            field = field.get_neighbor(o.lower)
 
     def expand(self, d: Direction, fields: Fs) -> bool:
         normal = d.default_orientation.normal
@@ -115,9 +144,11 @@ class Table(QuadLinkedList[F, OF]):
             return False
 
         link_nodes(normal.upper, adjacent_fields)
+        # merge_overlapping_fields(
+        #  d.default_orientation.normal, adjacent_fields)
         merge_small_fields(d.default_orientation, ref_fields, adjacent_fields)
 
-        head = self.insert_empty_fields_from_map(
+        head = insert_empty_fields_from_map(
             normal, ref_fields, adjacent_fields)
         self.insert(d, ref_fields[0], head)
 
@@ -154,46 +185,58 @@ class Table(QuadLinkedList[F, OF]):
         print("\n".join(lines))
 
 
-def group_fields_by2(fields: Fs, same_group_func: Callable[[F, F], bool],
-                     pre_sorter: str, group_sorter: str) -> list[Fs]:
+def group_fields_by(fields: Iterable[F],
+                    same_group_func: Callable[[F, F], bool],
+                    pre_sort_keys: str | Iterable[str] | None,
+                    group_sort_keys: str | Iterable[str] | None) -> list[Fs]:
     """ Group the given fields using the given function.
 
     :param fields: The fields that should be grouped.
     :param same_group_func: A function taking two fields and returning True,
       if they are in the same group, False otherwise.
-    :param pre_sorter: Sort the fields before grouping using this as key.
-    :param group_sorter: Each group will be sorted using this as key.
+    :param pre_sort_keys: Sort the fields before grouping using this as key.
+    :param group_sort_keys: Each group will be sorted using this as key.
     :return: A list of groups of fields.
     """
     groups: list[Fs] = []
-    fields = sorted(fields, key=attrgetter(pre_sorter))
+    if pre_sort_keys:
+        pre_sorter = attrgetter(*always_iterable(pre_sort_keys))
+        fields = sorted(fields, key=pre_sorter)
+
+    group_sorter = None
+    if group_sort_keys:
+        group_sorter = attrgetter(*always_iterable(group_sort_keys))
     for group in split_when(fields, same_group_func):
-        groups.append(sorted(group, key=attrgetter(group_sorter)))
+        if group_sorter:
+            group.sort(key=group_sorter)
+        groups.append(group)
 
     return groups
 
 
-def datafields_to_cols(data_fields: Fs) -> Cols:
+def fields_to_cols(fields: Fs, *, link_cols: bool = True) -> Cols:
     """ Turns the datafields into a collection of columns. """
     def _same_col(field1: F, field2: F) -> bool:
         """ Two fields are in the same column if they overlap horizontally. """
         return not field1.bbox.is_h_overlap(field2.bbox)
 
-    cols = group_fields_by2(data_fields, _same_col, "bbox.x0", "bbox.y0")
-    for col in cols:
-        link_nodes(S, col)
+    cols = group_fields_by(fields, _same_col, "bbox.x0", "bbox.y0")
+    if link_cols:
+        for col in cols:
+            link_nodes(S, col)
     return cols
 
 
-def datafields_to_rows(data_fields: Fs) -> Rows:
+def fields_to_rows(fields: Fs, *, link_rows: bool = True) -> Rows:
     """ Turns the datafields into a collection of rows. """
     def _same_row(field1: F, field2: F) -> bool:
         """ Two fields are in the same row if they overlap vertically. """
         return not field1.bbox.is_v_overlap(field2.bbox)
 
-    rows = group_fields_by2(data_fields, _same_row, "bbox.y0", "bbox.x0")
-    for row in rows:
-        link_nodes(E, row)
+    rows = group_fields_by(fields, _same_row, "bbox.y0", "bbox.x0")
+    if link_rows:
+        for row in rows:
+            link_nodes(E, row)
     return rows
 
 
@@ -201,6 +244,25 @@ def link_nodes(d: Direction, fields: Fs) -> None:
     p = peekable(fields)
     for field in p:
         field.set_neighbor(d, p.peek(None))
+
+
+def link_rows_and_cols(rows: list[Fs], cols: list[Fs]) -> None:
+    # Fill first column.
+    first_field_of_rows = [row[0] for row in rows]
+    head = insert_empty_fields_from_map(V, first_field_of_rows, cols[0])
+    cols[0] = list(head.iter(S))
+    rel_col = cols[0]
+    for col_id, col in enumerate(cols[1:], 1):
+        head = insert_empty_fields_from_map(V, rel_col, col)
+        cols[col_id] = list(head.iter(S))
+        rel_col = cols[col_id]
+    for col1, col2 in pairwise(cols):
+        for f1, f2 in zip(col1, col2, strict=True):
+            f1.set_neighbor(E, None)
+            f2.set_neighbor(E, None)
+            f1.set_neighbor(E, f2)
+    for row_id, field in enumerate(cols[0]):
+        rows[row_id] = list(field.iter(E))
 
 
 def insert_missing_empty_fields(rows: list[Fs], cols: list[Fs]) -> None:
@@ -215,6 +277,7 @@ def insert_missing_empty_fields(rows: list[Fs], cols: list[Fs]) -> None:
             first_field_of_rows[row_id].prev = empty_field
             row_id += 1
         row_id += 1
+    insert_empty_fields_from_map(V, first_field_of_rows, first_col)
     cols_top = [col[0] for col in cols]
     new_tops = {}
     for field1, field2 in pairwise(cols_top):
