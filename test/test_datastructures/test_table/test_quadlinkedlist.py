3306ffe24d3cf71ad247ae8ba0244bc052c1e742
Julius Heinzinger <julius.heinzinger@gmail.com>
2023-05-10 00:37:14 +0200

    Add simple node creation; Add a test

--- test/test_datastructures/test_table/test_quadlinkedlist.py
+++ test/test_datastructures/test_table/test_quadlinkedlist.py
@@ -4,14 +4,30 @@ from pdf2gtfs.datastructures.table.nodes import QuadNode
 from pdf2gtfs.datastructures.table.quadlinkedlist import QuadLinkedList
 
 
+def create_rectangle_nodes(num_col: int = 4, num_row: int = 5
+                           ) -> list[QuadNode]:
+    nodes = [QuadNode() for _ in range(num_col * num_row)]
+    for i in range(num_col * num_row):
+        if i % num_col:
+            nodes[i - 1].next = nodes[i]
+        if i >= num_col:
+            nodes[i - num_col].below = nodes[i]
+    return nodes
+
+
 class TestQuadLinkedList(TestCase):
     def setUp(self) -> None:
-        self.node1 = QuadNode()
-        self.node2 = QuadNode()
-        self.qll = QuadLinkedList(self.node1, self.node2)
+        self.nodes = create_rectangle_nodes()
+        self.qll = QuadLinkedList(self.nodes[0], self.nodes[-1])
 
     def test_properties(self) -> None:
-        self.skipTest("Not implemented yet!")
+        self.nodes = create_rectangle_nodes()
+        self.qll = QuadLinkedList(self.nodes[0], self.nodes[-1])
+        self.assertEqual(self.nodes[0], self.qll.left)
+        self.assertEqual(self.nodes[0], self.qll.top)
+        self.assertEqual(self.nodes[-1], self.qll.right)
+        self.assertEqual(self.nodes[-1], self.qll.bot)
+        # TODO: Add setter tests.
 
     def test_get_end_node(self) -> None:
         self.skipTest("Not implemented yet!")
